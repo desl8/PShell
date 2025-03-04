f50646e7aa0a4dd5e3ff9cc57cd85c2d13b6cb86
Kevin <127263761+desl8@users.noreply.github.com>
2025-03-04 13:06:14 -0800

    Update Rickroll-Multi.ps1
    Improvements :)

--- Short/Rickroll-Multi.ps1
+++ Short/Rickroll-Multi.ps1
@@ -1,4 +1,4 @@
-for ($i = 0; $i -lt 10; $i++) { 
+for ($i = 0; $i -lt 50; $i++) { 
     $WScript = New-Object -com wscript.shell; 1..50 | % { $WScript.SendKeys([char]175) }; 
-    Start-Process "msedge.exe" -ArgumentList '--new-window https://player.vimeo.com/video/375468729' | ForEach-Object { Start-Sleep -Seconds 2; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait("{F11}"); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait(" ") } 
+    Start-Process "msedge.exe" -ArgumentList '--new-window https://ajsya.github.io/rickroll/' | ForEach-Object { Start-Sleep -Seconds 2; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait("{F11}"); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait(" ") } 
 }
