3689ca817223fd582a5cfb43b18b779965720dde
Kevin <127263761+desl8@users.noreply.github.com>
2025-03-06 14:12:12 -0800

    Update Rickroll-Multi.ps1

--- Short/Rickroll-Multi.ps1
+++ Short/Rickroll-Multi.ps1
@@ -1,4 +1,4 @@
 for ($i = 0; $i -lt 20; $i++) { 
     $WScript = New-Object -com wscript.shell; 1..50 | % { $WScript.SendKeys([char]175) }; 
-    Start-Process "msedge.exe" -ArgumentList '--new-window https://shattereddisk.github.io/rickroll/rickroll.mp4' | ForEach-Object { Start-Sleep -Seconds 2; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait("{F11}"); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait(" ") } 
+    Start-Process "msedge.exe" -ArgumentList '--new-window https://youtu.be/xMHJGd3wwZk' | ForEach-Object { Start-Sleep -Seconds 2; Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait("{F11}"); Start-Sleep -Milliseconds 500; [System.Windows.Forms.SendKeys]::SendWait(" ") } 
 }
